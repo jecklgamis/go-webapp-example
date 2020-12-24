673034c6cc9f0ddc99b3fade5fb79ea663f58fe2
Jerrico Gamis <jecklgamis@gmail.com>
2020-12-25 10:55:53 +1100

    Check for binaries

--- reloader/reloader.sh
+++ reloader/reloader.sh
@@ -2,12 +2,18 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+function check_binaries() {
+  hash go || echo "go binary not found!" && exit -1
+  hash fswatch || echo "fswatch binary not found!" && exit -1
+}
+
 function sigint_handler() {
   ${SCRIPT_DIR}/kill-app.sh
 }
 
 trap 'sigint_handler' SIGINT
 
+check_binaries
 ${SCRIPT_DIR}/build-app.sh
 ${SCRIPT_DIR}/kill-app.sh
 ${SCRIPT_DIR}/run-app.sh
