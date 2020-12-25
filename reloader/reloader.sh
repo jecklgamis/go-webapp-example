0decb68e16897fb479abc75a104e72910bd658d7
Jerrico Gamis <jecklgamis@gmail.com>
2020-12-25 11:11:29 +1100

    Fix reloader.sh

--- reloader/reloader.sh
+++ reloader/reloader.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+APP_DIR=${SCRIPT_DIR}/..
 
 function check_binaries() {
-  hash go || echo "go binary not found!" && exit -1
-  hash fswatch || echo "fswatch binary not found!" && exit -1
+  if ! command -v go &>/dev/null; then
+    echo "go binary not found!"
+    exit -1
+  fi
+  if ! command -v fswatch &>/dev/null; then
+    echo "fswatch binary not found!"
+    exit -1
+  fi
 }
 
 function sigint_handler() {
@@ -17,4 +24,4 @@ check_binaries
 ${SCRIPT_DIR}/build-app.sh
 ${SCRIPT_DIR}/kill-app.sh
 ${SCRIPT_DIR}/run-app.sh
-fswatch -o pkg -o cmd | xargs -n1 -I{} sh -c "${SCRIPT_DIR}/build-app.sh && ${SCRIPT_DIR}/kill-app.sh && ${SCRIPT_DIR}/run-app.sh"
+fswatch -o ${APP_DIR}/pkg -o ${APP_DIR}/cmd | xargs -n1 -I{} sh -c "${SCRIPT_DIR}/build-app.sh && ${SCRIPT_DIR}/kill-app.sh && ${SCRIPT_DIR}/run-app.sh"
