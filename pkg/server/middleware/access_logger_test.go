a377d9a0e79707e8982c8feaeaafe630d1b101af
Jerrico Gamis <jecklgamis@gmail.com>
2020-12-26 11:03:10 +1100

    Add access logger middleware test

---
+++ pkg/server/middleware/access_logger_test.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	test "github.com/jecklgamis/go-api-server-template/pkg/test"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestInvokeNextHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	test.Assert(t, err == nil, "Unable to create request")
+
+	nextHandlerInvoked := false
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		nextHandlerInvoked = true
+	})
+	rr := httptest.NewRecorder()
+	handler := AccessLoggerMiddleware(nextHandler)
+	handler.ServeHTTP(rr, req)
+	test.Assert(t, nextHandlerInvoked, "Expecting next handler invocation")
+}
