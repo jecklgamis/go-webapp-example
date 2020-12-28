ab0244e02de8d22af03d53b90f0bc7b8f8991aa7
Jerrico Gamis <jecklgamis@gmail.com>
2020-12-28 14:10:52 +1100

    Display routes on startup

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -11,6 +11,17 @@ import (
 	"time"
 )
 
+func displayRoutes(router *mux.Router) {
+	log.Println("Below are the configured endpoints")
+	_ = router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		template, err := route.GetPathTemplate()
+		if err == nil {
+			log.Println(template)
+		}
+		return nil
+	})
+}
+
 func Start() {
 	env := GetEnvOrElse("APP_ENVIRONMENT", "dev")
 	config := ReadConfig(env)
@@ -21,6 +32,7 @@ func Start() {
 	router.HandleFunc("/api", handler.ApiHandler)
 	router.HandleFunc("/", handler.RootHandler)
 	router.Use(middleware.AccessLoggerMiddleware)
+	displayRoutes(router)
 
 	if config.Server.Https != nil {
 		go func() {
